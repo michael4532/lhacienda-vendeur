4b733c329488aec2ffc18a737a1ad29a730ee154
DevGenestar <dev@genestartechnologies.com>
2024-08-20 18:30:21 +0000

    Initial Commit

---
+++ Target Support Files/flutter_pos_printer_platform_image_3_sdt/flutter_pos_printer_platform_image_3_sdt-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_flutter_pos_printer_platform_image_3_sdt : NSObject
+@end
+@implementation PodsDummy_flutter_pos_printer_platform_image_3_sdt
+@end
---
+++ Target Support Files/flutter_pos_printer_platform_image_3_sdt/flutter_pos_printer_platform_image_3_sdt-umbrella.h
@@ -0,0 +1,22 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "BLEConnecter.h"
+#import "Connecter.h"
+#import "ConnecterBlock.h"
+#import "ConnecterManager.h"
+#import "EthernetConnecter.h"
+#import "FlutterPosPrinterPlatformPlugin.h"
+
+FOUNDATION_EXPORT double flutter_pos_printer_platform_image_3_sdtVersionNumber;
+FOUNDATION_EXPORT const unsigned char flutter_pos_printer_platform_image_3_sdtVersionString[];
+
